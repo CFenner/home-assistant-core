39d37cb48aab8ffe0859fe911e520ba0cbf6b51b
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2023-07-07 01:01:03 +0200

    remove unused import

--- homeassistant/components/vicare/climate.py
+++ homeassistant/components/vicare/climate.py
@@ -33,7 +33,6 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_platform
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
