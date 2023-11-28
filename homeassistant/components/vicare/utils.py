6ba47bbcb61159a91f30a50075495d91b68925ee
Christopher Fenner <Christopher.Fenner@me.com>
2023-11-28 15:06:05 +0100

    create helper

--- homeassistant/components/vicare/utils.py
+++ homeassistant/components/vicare/utils.py
@@ -2,12 +2,16 @@
 import logging
 
 from PyViCare.PyViCareDevice import Device as PyViCareDevice
+from PyViCare.PyViCareDeviceConfig import PyViCareDeviceConfig
 from PyViCare.PyViCareHeatingDevice import (
     HeatingDeviceWithComponent as PyViCareHeatingDeviceComponent,
 )
 from PyViCare.PyViCareUtils import PyViCareNotSupportedFeatureError
 
+from homeassistant.config_entries import ConfigEntry
+
 from . import ViCareRequiredKeysMixin
+from .const import CONF_HEATING_TYPE, HEATING_TYPE_TO_CREATOR_METHOD, HeatingType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,6 +32,16 @@ def is_supported(
         _LOGGER.debug("Attribute Error %s: %s", name, error)
         return False
     return True
+
+
+def get_device(
+    device_config: PyViCareDeviceConfig, entry: ConfigEntry
+) -> PyViCareDevice:
+    """Get device for device config."""
+    return getattr(
+        device_config,
+        HEATING_TYPE_TO_CREATOR_METHOD[HeatingType(entry.data[CONF_HEATING_TYPE])],
+    )()
 
 
 def get_burners(device: PyViCareDevice) -> list[PyViCareHeatingDeviceComponent]:
